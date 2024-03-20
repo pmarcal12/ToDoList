58fadc9f9b984a9f1e19cc59fe13e3ccd244d3e2
Pedro Marçal <pedromarcal.1995@gmail.com>
2024-03-20 17:53:26 +0000

    ToDoList.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,6 @@ import TodoList from './TodoList';
 
 function App() {
   return (
-    // <div className="App">
-    //   {/* <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
-    //   <TodoList />
-    //   <input type="text"/>
-    //   <button>Add Task</button>
-    //   <button>Clear Completed</button>
-    //   <p>x tasks left</p>
-    // </div>
     <>
       <TodoList />
       <input type="text"/>
